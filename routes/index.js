1bb256e04d79c50e4068bf9ea36f82d993905e1e
abe-sinjiro <real_striver@outlook.com>
2021-11-29 00:40:22 +0700

    block hash endpoint done

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
-const web3 = require('web3');
+const Web3 = require('web3');
 const router = express.Router();
 
+const web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/570f1ebd62024227a90b259a6e718de0"));
+
 /* GET home page. */
-router.post('/', (req, res) => {
+router.get('/', (req, res) => {
+  res.json({"message": "server is running"});
+});
+
+router.post('/', async (req, res) => {
   const postData = req.body;
-  console.log(web3.eth.getBlock(postData.blockNumber).hash);
-  res.json({'message': "Server is running"});
+  const selBlock = await web3.eth.getBlock(postData.blockNumber);
+  res.json({'hash': selBlock.hash});
 });
 
 module.exports = router;
