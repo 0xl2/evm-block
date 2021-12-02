0d25a401ccdd2a82ec186d80c139e3e6f2835e11
abe-sinjiro <real_striver@outlook.com>
2021-12-02 21:29:48 +0700

    eth frontend completed

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,71 @@
 const express = require('express');
-const Web3 = require('web3');
+// const Tx = require('ethereumjs-tx').Transaction;
+const Tx = require('ethereumjs-tx');
 const request = require('request');
-const router = express.Router();
+const empty = require('is-empty');
+const Validator = require('validatorjs');
 
 const config = require("../config/config.json");
 
-const web3 = new Web3(new Web3.providers.HttpProvider(config.network));
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
   res.json({"message": "server is running"});
 });
 
-router.post('/', async (req, res) => {
+router.post('/', (req, res) => {
   const postData = req.body;
-  const selBlock = await web3.eth.getBlock(postData.blockNumber);
-  res.json({'hash': selBlock.hash});
+  const validator = new Validator(postData, {
+    from: 'required|string',
+    receiver: 'required|string',
+    amount: 'required',
+    network: 'required|string'
+  });
+  
+  if(validator.fails()) {
+    return res.status(401).json({
+      type: 'ValidationError',
+      errors: validation.errors.all(),
+    });
+  } else {
+    try {
+      request.post({
+        url: 'http://localhost:3000/ethers/transfer_request',
+        json: postData
+      }, (error, req, resp) => {
+        if(error) {
+          return res.status(500).json({err: error.toString()});
+        } else {
+          if(!empty(resp.rawTransaction)) {
+            const tx = new Tx(resp.rawTransaction);
+            const privKey = Buffer.from(config.private_key, 'hex');
+            tx.sign(privKey);
+            
+            const serializedTx = `0x${tx.serialize().toString('hex')}`;
+
+            request.post({
+              url: 'http://localhost:3000/ethers/transfer_broadcast',
+              json: {
+                rawTransaction: serializedTx,
+                network: postData.network
+              }
+            }, (err, req1, resp1) => { 
+              if(err) {
+                return res.status(500).json({err: err.toString()});
+              } else {
+                return res.json(resp1);
+              }
+            });
+          } else {
+            return res.status(500).json({err: 'invalid raw transaction'});
+          }
+        }
+      });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  }
 });
 
 module.exports = router;
