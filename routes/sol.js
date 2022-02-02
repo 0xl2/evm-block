50d8951d47c20f3558c9d31df1321a16783843bc
DESKTOP-MOTPEKH\REAL <real_striver@outlook.com>
2022-02-03 06:57:11 +0900

    spl and nft transfer working

--- routes/sol.js
+++ routes/sol.js
@@ -37,7 +37,6 @@ router.post('/', (req, res) => {
                 json: postData
             }, async (error, req, resp) => {
                 if(error) {
-                    console.log(error);
                     return res.status(500).json({err: error.toString()});
                 } else {
                     if(!empty(resp.blockhash)) {
@@ -67,7 +66,6 @@ router.post('/', (req, res) => {
                             }
                         }, (err, req1, resp1) => { 
                             if(err) {
-                                console.log(err);
                                 return res.status(500).json({err: err.toString()});
                             } else {
                                 return res.json(resp1);
@@ -93,6 +91,7 @@ router.post('/spl', (req, res) => {
         receiver: 'required|string',
         amount: 'required',
         programId: 'required|string',
+        mintAddress: 'required|string',
         network: 'required|string'
     });
 
@@ -108,12 +107,27 @@ router.post('/spl', (req, res) => {
                 json: postData
             }, async (error, req, resp) => {
                 if(error) {
-                    console.log(error);
                     return res.status(500).json({err: error.toString()});
                 } else {
                     if(!empty(resp.blockhash) && !empty(resp.decimals)) {
                         const uint8Arr = new Uint8Array(Base58.decode(config.solana_key));
                         const fromWallet = solanaWeb3.Keypair.fromSecretKey(uint8Arr);
+
+                        const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl("testnet"));
+                        const myToken = new Token(
+                            connection,
+                            new solanaWeb3.PublicKey(postData.programId),
+                            new solanaWeb3.PublicKey(postData.mintAddress),
+                            fromWallet
+                        );
+
+                        const fromTokenAccount = await myToken.getOrCreateAssociatedAccountInfo(
+                            new solanaWeb3.PublicKey(postData.from)
+                        );
+
+                        const toTokenAccount = await myToken.getOrCreateAssociatedAccountInfo(
+                            new solanaWeb3.PublicKey(postData.receiver)
+                        );
 
                         const manualTransaction = new solanaWeb3.Transaction({
                             recentBlockhash: resp.blockhash,
@@ -121,21 +135,17 @@ router.post('/spl', (req, res) => {
                         });
                         manualTransaction.add(
                             Token.createTransferInstruction(
-                                new solanaWeb3.PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"),
-                                new solanaWeb3.PublicKey(postData.from),
-                                new solanaWeb3.PublicKey(postData.receiver),
+                                new solanaWeb3.PublicKey(postData.mintAddress),
+                                fromTokenAccount.address,
+                                toTokenAccount.address,
                                 fromWallet.publicKey,
                                 [],
-                                postData.amount * Math.pow(10, resp.decimals)
+                                postData.amount * Math.pow(10, 9)
                             )
                         );
                         
                         const signature = nacl.sign.detached(new Uint8Array(manualTransaction.serializeMessage()), fromWallet.secretKey);
                         manualTransaction.addSignature(fromWallet.publicKey, new Buffer.from(signature));
-                        
-                        console.log(`The signatures were verifed: ${manualTransaction.verifySignatures()}`)
-
-                        solanaWeb3.
 
                         request.post({
                             url: 'http://localhost:3000/solana/transfer_broadcast',
@@ -145,7 +155,96 @@ router.post('/spl', (req, res) => {
                             }
                         }, (err, req1, resp1) => { 
                             if(err) {
-                                console.log(err);
+                                return res.status(500).json({err: err.toString()});
+                            } else {
+                                return res.json(resp1);
+                            }
+                        });
+                    } else {
+                        return res.status(500).json({error: "Invalid unsigned transaction"});
+                    }
+                }
+            }, (err) => {
+                return res.status(500).json({ error: error.toString() });
+            });
+        } catch (error) {
+            return res.status(500).json({ error: error.message });
+        }
+    }
+});
+
+router.post('/nft', (req, res) => {
+    const postData = req.body;
+    const validator = new Validator(postData, {
+        from: 'required|string',
+        receiver: 'required|string',
+        amount: 'required',
+        programId: 'required|string',
+        mintAddress: 'required|string',
+        network: 'required|string'
+    });
+
+    if(validator.fails()) {
+        return res.status(401).json({
+            type: 'ValidationError',
+            errors: validator.errors.all(),
+        });
+    } else {
+        try {
+            request.post({
+                url: 'http://localhost:3000/solana/transfer_request',
+                json: postData
+            }, async (error, req, resp) => {
+                if(error) {
+                    return res.status(500).json({err: error.toString()});
+                } else {
+                    if(!empty(resp.blockhash)) {
+                        const uint8Arr = new Uint8Array(Base58.decode(config.solana_key));
+                        const fromWallet = solanaWeb3.Keypair.fromSecretKey(uint8Arr);
+
+                        const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl("devnet"));
+                        const myToken = new Token(
+                            connection,
+                            new solanaWeb3.PublicKey(postData.programId),
+                            new solanaWeb3.PublicKey(postData.mintAddress),
+                            fromWallet
+                        );
+
+                        const fromTokenAccount = await myToken.getOrCreateAssociatedAccountInfo(
+                            new solanaWeb3.PublicKey(postData.from)
+                        );
+
+                        const toTokenAccount = await myToken.getOrCreateAssociatedAccountInfo(
+                            new solanaWeb3.PublicKey(postData.receiver)
+                        );
+
+                        const manualTransaction = new solanaWeb3.Transaction({
+                            recentBlockhash: resp.blockhash,
+                            feePayer: fromWallet.publicKey
+                        });
+                        manualTransaction.add(
+                            Token.createTransferInstruction(
+                                new solanaWeb3.PublicKey(postData.mintAddress),
+                                fromTokenAccount.address,
+                                toTokenAccount.address,
+                                fromWallet.publicKey,
+                                [],
+                                // 1000000,
+                                1
+                            )
+                        );
+                        
+                        const signature = nacl.sign.detached(new Uint8Array(manualTransaction.serializeMessage()), fromWallet.secretKey);
+                        manualTransaction.addSignature(fromWallet.publicKey, new Buffer.from(signature));
+
+                        request.post({
+                            url: 'http://localhost:3000/solana/transfer_broadcast',
+                            json: {
+                                rawTransaction: manualTransaction.serialize(),
+                                network: postData.network
+                            }
+                        }, (err, req1, resp1) => { 
+                            if(err) {
                                 return res.status(500).json({err: err.toString()});
                             } else {
                                 return res.json(resp1);
